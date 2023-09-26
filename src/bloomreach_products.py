48c4d925cee60fa566ee06550b64532c12884ad0
s-bhat-BR <146017600+s-bhat-BR@users.noreply.github.com>
2023-09-26 11:24:29 -0700

    Update bloomreach_products.py
    change id, attribute in create_product

--- src/bloomreach_products.py
+++ src/bloomreach_products.py
@@ -25,8 +25,8 @@ def create_products(fp, shopify_url):
 def create_product(product, shopify_url):
 
   out_product = {
-    "id": product["id"],
-    "attributes": product["attributes"].copy(),
+    "id": product["spm.custom.br_product_id"],
+    "attributes": product["attributes"]["sp.totalInventory"],
     "variants": {}
   }
 
