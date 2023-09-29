f1de2d929b2f1003ae9ccb2462e0e912d7925d2c
s-bhat-BR <146017600+s-bhat-BR@users.noreply.github.com>
2023-09-29 02:02:33 +0000

    Pending changes exported from your codespace

--- src/bloomreach_products.py
+++ src/bloomreach_products.py
@@ -13,149 +13,158 @@ PRODUCT_MAPPINGS = [
 
 
 def create_products(fp, shopify_url):
-  products = []
+    """Function to invoke BR discovery products on each record"""
+    products = []
 
-  with gzip.open(fp, 'rb') as file:
-    for line in file:
-      products.append(create_product(json.loads(line), shopify_url))
+    with gzip.open(fp, 'rb') as file:
+        for line in file:
+            products.append(create_product(json.loads(line), shopify_url))
 
-  return products
+    return products
 
 
 def create_product(product, shopify_url):
+    """Function creating BR discovery product"""
+    out_product = {
+      "id": product["id"],
+      "attributes": product["attributes"].copy(),
+      "variants": {}
+    }
 
-  out_product = {
-    "id": product["id"],
-    "attributes": product["attributes"].copy(),
-    "variants": {}
-  }
+    # out_product["id"] = False
+    # container for input product attributes
+    in_pa = product["attributes"]
 
-  # container for input product attributes
-  in_pa = product["attributes"]
-  
-  # container for the transformed product attributes and variants
-  # these dictionaries will be merged into out_product at the end
-  out_pa = out_product["attributes"]
+    # container for the transformed product attributes and variants
+    # these dictionaries will be merged into out_product at the end
+    out_pa = out_product["attributes"]
 
-  if in_pa["sp.status"] == "ACTIVE" and "sp.totalInventory" in in_pa and in_pa["sp.totalInventory"] > 0:
-    out_pa["availability"] = True
-  else:
-    out_pa["availability"] = False
-
-  # process product level mappings
-  for mapping in PRODUCT_MAPPINGS:
-    source, dest, func = mapping[0], mapping[1], mapping[2]
-    # handle scenario when pa[source] is false, such as 0
-    if source in in_pa:
-      value = func(in_pa[source])
-      if type(value) == str and value == "":
-        # don't add empty string values
-        pass
-      else:
-        out_pa[dest] = value
-
-  # iterate over each variant
-  for v_id, variant in product["variants"].items():
-
-    # container of input and output variant attributes
-    in_va = variant["attributes"]
-    out_va = variant["attributes"].copy()
-    out_product["variants"][v_id] = {}
-
-    if "sv.compareAtPrice" in in_va and in_va["sv.compareAtPrice"]:
-      if in_va["sv.compareAtPrice"] == in_va["sv.price"]:
-        out_va["price"] = in_va["sv.compareAtPrice"]
-      else:
-        out_va["price"] = in_va["sv.compareAtPrice"]
-        out_va["sale_price"] = in_va["sv.price"]
+    if in_pa["sp.status"] == "ACTIVE" and "sp.totalInventory" in in_pa and in_pa["sp.totalInventory"] > 0:
+        out_pa["availability"] = True
     else:
-      out_va["price"] = in_va["sv.price"]
+        out_pa["availability"] = False
 
-    # set color, size from selectedOptions
-    # TODO: set other options to custom attributes
-    if "sv.selectedOptions" in in_va:
-      if in_va["sv.selectedOptions"] and len(in_va["sv.selectedOptions"]) > 0:
-        for option in in_va["sv.selectedOptions"]:
-          if "name" in option and "value" in option and "Color" in option[
-              "name"]:
-            out_va["color"] = option["value"]
-            # br_va["variants_color"] = option["value"]
-          if "name" in option and "value" in option and "Size" in option[
-              "name"]:
-            out_va["size"] = option["value"]
+    # process product level mappings
+    for mapping in PRODUCT_MAPPINGS:
+        source, dest, func = mapping[0], mapping[1], mapping[2]
+        # handle scenario when pa[source] is false, such as 0
+        if source in in_pa:
+            value = func(in_pa[source])
+            if isinstance(value, str) and value == "":
+              # don't add empty string values
+                pass
+            else:
+                out_pa[dest] = value
 
-    out_va["availability"] = False
-    if "sv.availableForSale" in in_va and in_va["sv.availableForSale"]:
-      out_va["availability"] = True
-    else:
-      out_va["availability"] = False
+      # iterate over each variant
+    variant_ids = []
+    for v_id, variant in product["variants"].items():
+        variant_ids.append(v_id)
+        # container of input and output variant attributes
+        in_va = variant["attributes"]
+        out_va = variant["attributes"].copy()
+        out_product["variants"][v_id] = {}
 
-    # set thumb_image (swatch_image isn't a standard shopify concept as far as I can tell)
-    # https://shopify.dev/api/admin-graphql/2023-01/objects/ProductVariant#field-productvariant-image
-    if "sv.image" in in_va and in_va["sv.image"] and "url" in in_va["sv.image"]:
-      out_va["thumb_image"] = in_va["sv.image"]["url"]
-    
-    out_product["variants"][v_id]["attributes"] = out_va
+        if "sv.compareAtPrice" in in_va and in_va["sv.compareAtPrice"]:
+            if in_va["sv.compareAtPrice"] == in_va["sv.price"]:
+                out_va["price"] = in_va["sv.compareAtPrice"]
+            else:
+                out_va["price"] = in_va["sv.compareAtPrice"]
+                out_va["sale_price"] = in_va["sv.price"]
+        else:
+            out_va["price"] = in_va["sv.price"]
 
-  return out_product
+        # set color, size from selectedOptions
+        # TODO: set other options to custom attributes
+        if "sv.selectedOptions" in in_va:
+            if in_va["sv.selectedOptions"] and len(in_va["sv.selectedOptions"]) > 0:
+                for option in in_va["sv.selectedOptions"]:
+                    if "name" in option and "value" in option and "Color" in option["name"]:
+                        out_va["color"] = option["value"]
+                  # br_va["variants_color"] = option["value"]
+                    if "name" in option and "value" in option and "Size" in option["name"]:
+                        out_va["size"] = option["value"]
+
+        out_va["availability"] = False
+        if "sv.availableForSale" in in_va and in_va["sv.availableForSale"]:
+            out_va["availability"] = True
+        else:
+            out_va["availability"] = False
+
+        # set thumb_image (swatch_image isn't a standard shopify concept as far as I can tell)
+        # https://shopify.dev/api/admin-graphql/2023-01/objects/ProductVariant#field-productvariant-image
+        if "sv.image" in in_va and in_va["sv.image"] and "url" in in_va["sv.image"]:
+            out_va["thumb_image"] = in_va["sv.image"]["url"]
+        out_product["variants"][v_id]["attributes"] = out_va
+
+    out_product["id"] = variant_ids[0]
+    return out_product
 
 
 def main(fp_in, fp_out, shopify_url):
-  patch = create_products(fp_in, shopify_url)
+    """Main method"""
+    patch = create_products(fp_in, shopify_url)
 
-  # write JSONLines
-  with gzip.open(fp_out, "wb") as file:
-    writer = jsonlines.Writer(file)
-    for object in patch:
-      writer.write(object)
-    writer.close()
+      # write JSONLines
+    with gzip.open(fp_out, "wb") as file:
+        writer = jsonlines.Writer(file)
+        for object in patch:
+            writer.write(object)
+        writer.close()
 
 
 if __name__ == '__main__':
-  import argparse
-  from os import getenv
+    import argparse
+    from os import getenv
 
-  from sys import stdout
-  
-  # Define logger
-  loglevel = getenv('LOGLEVEL', 'INFO').upper()
-  logging.basicConfig(
-    stream=stdout, 
-    level=loglevel,
-    format="%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s"
-  )
-  
-  parser = argparse.ArgumentParser(
-    description="Transforms generic products with custom logic specific to an individual catalog. This is more or less a place holder script to add any transformations necessary that need to be made on top of the generic product transforms. For instance, if shopify product tags are used in a special way, custom transforms can be created. Also, generic transforms can be overriden should it be necessary for a catalog specific behavior. The values of the shopify prefixed attributes should not be modified."
-  )
-  
-  parser.add_argument(
-    "--input-file",
-    help="File path of Generic Products jsonl",
-    type=str,
-    default=getenv("BR_INPUT_FILE"),
-    required=not getenv("BR_INPUT_FILE")
-  )
+    from sys import stdout
 
-  parser.add_argument(
-    "--output-file",
-    help="Filename of output jsonl file",
-    type=str,
-    default=getenv("BR_OUTPUT_FILE"),
-    required=not getenv("BR_OUTPUT_FILE")
-  )
+    # Define logger
+    loglevel = getenv('LOGLEVEL', 'INFO').upper()
+    logging.basicConfig(
+      stream=stdout,
+      level=loglevel,
+      format="%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s"
+    )
 
-  parser.add_argument(
-    "--shopify-url",
-    help="Hostname of the shopify Shop, e.g. xyz.myshopify.com.",
-    type=str,
-    default=getenv("BR_SHOPIFY_URL"),
-    required=not getenv("BR_SHOPIFY_URL")
-  )
+    parser = argparse.ArgumentParser(
+      description="Transforms generic products with custom logic specific to an individual catalog."
+      "This is more or less a place holder script to add any transformations necessary"
+      "that need to be made on top of the generic product transforms."
+      "For instance, if shopify product tags are used in a special way,"
+      "custom transforms can be created."
+      "Also, generic transforms can be overriden should it be necessary"
+      "for a catalog specific behavior."
+      "The values of the shopify prefixed attributes should not be modified."
+    )
 
-  args = parser.parse_args()
-  fp_in = args.input_file
-  fp_out = args.output_file
-  shopify_url = args.shopify_url
+    parser.add_argument(
+      "--input-file",
+      help="File path of Generic Products jsonl",
+      type=str,
+      default=getenv("BR_INPUT_FILE"),
+      required=not getenv("BR_INPUT_FILE")
+    )
 
-  main(fp_in, fp_out, shopify_url)
+    parser.add_argument(
+      "--output-file",
+      help="Filename of output jsonl file",
+      type=str,
+      default=getenv("BR_OUTPUT_FILE"),
+      required=not getenv("BR_OUTPUT_FILE")
+    )
+
+    parser.add_argument(
+      "--shopify-url",
+      help="Hostname of the shopify Shop, e.g. xyz.myshopify.com.",
+      type=str,
+      default=getenv("BR_SHOPIFY_URL"),
+      required=not getenv("BR_SHOPIFY_URL")
+    )
+
+    args = parser.parse_args()
+    fp_in = args.input_file
+    fp_out = args.output_file
+    shopify_url = args.shopify_url
+
+    main(fp_in, fp_out, shopify_url)
